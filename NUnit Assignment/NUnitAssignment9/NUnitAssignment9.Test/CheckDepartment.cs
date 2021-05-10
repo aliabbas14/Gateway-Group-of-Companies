48cce5057d8ab911ecffc279912696167d144d37
aliabbas14 <68897039+aliabbas14@users.noreply.github.com>
2021-05-10 18:17:54 +0530

    NUnitAssignment9

---
+++ NUnit Assignment/NUnitAssignment9/NUnitAssignment9.Test/CheckDepartment.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitAssignment9.Test
+{
+    public class CheckDepartment : NUnit.Framework.Constraints.Constraint
+    {
+        readonly string _dept;
+
+        public CheckDepartment(string dept)
+        {
+            _dept = dept;
+
+        }
+        public override ConstraintResult ApplyTo<TActual>(TActual actual)
+        {
+            List<Student> stu = actual as List<Student>;
+            foreach (Student e in stu)
+            {
+                if (e.Department != _dept)
+                {
+                    return new ConstraintResult(this, actual, ConstraintStatus.Error);
+                }
+            }
+            return new ConstraintResult(this, actual, ConstraintStatus.Success);
+
+        }
+    }
+
+    public class Is : NUnit.Framework.Is
+    {
+        public static CheckDepartment Check(string dept)
+        {
+            return new CheckDepartment(dept);
+        }
+    }
+}
